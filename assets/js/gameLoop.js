80b71b808e30a36ad8af27b65cbfaec97ab2ed02
Ray Merrell <RayMerrell@outlook.com>
2023-03-23 17:04:07 +0000

    M46Course

--- assets/js/gameLoop.js
+++ assets/js/gameLoop.js
@@ -10,7 +10,8 @@ document.onload = (() => {
 
 //Instance Object
 //instantiate pet based on user input
-function makePet(){
+function makePet(){    
+    console.log("makepet");
     let strName= localStorage.getItem("name");
     let strType =localStorage.getItem("type");
     switch (strType) {
@@ -33,59 +34,76 @@ function makePet(){
 
 // Button Code
 //react to button presses by calling appropriate update methods of objPet
-document.getElementById("idButtonName1").addEventListener("click", fnUpdateThing1());
-document.getElementById("idButtonName2").addEventListener("click", fnUpdateThing2());
-document.getElementById("idButtonName3").addEventListener("click", fnUpdateThing3());
-document.getElementById("idButtonName4").addEventListener("click", fnUpdateThing4());
+document.getElementById("idFeed").addEventListener("click", fnUpdateHunger());
+document.getElementById("idWater").addEventListener("click", fnUpdateThirst());
+document.getElementById("idPlay").addEventListener("click", fnUpdateBoredom());
+document.getElementById("idPet").addEventListener("click", fnUpdateHappiness());
 
-function fnUpdateThing1(){
-    objPet.updateThing1();
+function fnUpdateHunger(){
+    objPet.UpdateHunger();
+    IncreaseHealth();
+    updateMaxTimer();
 };
-function fnUpdateThing2(){
-    objPet.updateThing2();
+function fnUpdateThirst(){
+    objPet.UpdateThirst();
+    IncreaseHealth();
+    updateMaxTimer();
 };
-function fnUpdateThing3(){
-    objPet.updateThing3();
+function fnUpdateBoredom(){
+    objPet.UpdateBoredom();
+    IncreaseHealth();
+    updateMaxTimer();
 };
-function fnUpdateThing4(){
-    objPet.updateThing4();
+function fnUpdateHappiness(){
+    objPet.UpdateHappiness();
+    IncreaseHealth();
+    updateMaxTimer();
+};
+function IncreaseHealth(){
+    objPet.IncreaseHealth();
+    updateMaxTimer();
 };
 //event timer
 function updateMaxTimer(){
     //integer value from (percentage)floats * max Possible timer value
-    intMaxTime = Math.round(objPet.intMaxTime * (1-objPet.fBoredom) * (1-objPet.fThirst) * (1-objPet.fHunger) * objPet.happiness);
+    intMaxTime = Math.round(objPet.intMaxTime * (1.1-objPet.Boredom) * (1.1-objPet.Thirst) * (1.1-objPet.Hunger) * (objPet.happiness +0.1));
+    intTimer = intMaxTime;
+    document.getElementById(idTimer).value = intTimer;
 };
 function fnUpdatePet(){
     objPet.boredom += fIncrease; //everything increases with every timer tick
     objPet.thirst += fIncrease;
     objPet.hunger += fIncrease;
     objPet.Happiness -= fIncrease; //except happiness and health which decreases
-    objPet.Health-= fIncrease;
+    objPet.Health -= fIncrease;
     //check death condition
-    if (dead){
+    if (objPet.Health <= 0){
         window.location="deathScreen.html";
-    }
+    };
 };
 function fnUpdateDisplay(){    //Interface Updates
-    document.getElementById("idDisplayName1").style.width = objPet.thing1Value * document.getElementById("idDisplayName1").style.maxWidth;
-    document.getElementById("idDisplayName2").style.width = objPet.thing2Value * document.getElementById("idDisplayName2").style.maxWidth;
-    document.getElementById("idDisplayName3").style.width = objPet.thing3Value * document.getElementById("idDisplayName3").style.maxWidth;
-    document.getElementById("idDisplayName4").style.width = objPet.thing4Value * document.getElementById("idDisplayName4").style.maxWidth;
-    document.getElementById("idTimerDisplay").style.value = intTimer; //add formatting if needed
+    document.getElementById("idHunger").style.width = objPet.Hunger * document.getElementById("idHunger").style.maxWidth;
+    document.getElementById("idThirst").style.width = objPet.Thirst * document.getElementById("idThirst").style.maxWidth;
+    document.getElementById("idBoredom").style.width = objPet.Boredom * document.getElementById("idBoredom").style.maxWidth;
+    document.getElementById("idHappiness").style.width = objPet.Happiness * document.getElementById("idHappiness").style.maxWidth;
+    document.getElementById("idHealth").style.width = objPet.Health * document.getElementById("idHealth").style.maxWidth;
 
-    document.getElementById("idTimerDisplay").value = objPet.age; //add formatting if needed
+    document.getElementById("idTimerDisplay").style.value = intTimer; //add formatting if needed
+    document.getElementById("idAge").value = objPet.age; //add formatting if needed
 };
 function doTime(){
     intTimer -=1;
-    elTimer.value = intTimer; //maybe needs formatting too
     objPet.age += 1; //update age of pet
     if (intTimer <= 0){ //timer reaches zero :- Update pet properties
         fnUpdatePet(); //increase hunger, thirst, boredom, decrease health and happiness
         fnUpdateDisplay(); //include timer and age in this function
+        intTimer = intMaxTime;
     };
+    document.getElementById(idTimer).value = intTimer; //maybe needs formatting too
     setInterval(doTime, 1000); //run timer check every 1 second
 };
 function start(){
+    console.log("Start");
     makePet(); //instantiate pet
     fnUpdateDisplay(); //draw display values
     setInterval(doTime, 1000); //start the game loop
--- assets/js/start.js
+++ assets/js/start.js
@@ -1,8 +1,42 @@
-startButton = document.getElementById("idStart");
-startButton.addEventListener("click", startGame());
+let strType="";
+console.log("script loaded");
+document.getElementById("idStart").addEventListener("click", startGame);
 
-function startGame(){
-    localStorage.setItem("name", document.getElementById("idName").value);
-    localStorage.setItem("type", document.getElementById("idType").value);//or whatever to get the data
-    window.location = "MainGame.html"; //or whatever it's called
+
+document.getElementById("idSunFish").addEventListener("click", ()=>{
+    strType = "SunFish"
+    localStorage.setItem("type", strType );    
+    console.log(strType);
+});
+document.getElementById("idShrimp").addEventListener("click", ()=>{
+    strType = "Shrimp"
+    localStorage.setItem("type", strType);
+    console.log(strType);
+});
+document.getElementById("idTriggerFish").addEventListener("click", ()=>{
+    strType = "TriggerFish"
+    localStorage.setItem("type", strType);
+    console.log(strType);
+});
+document.getElementById("idCrab").addEventListener("click", ()=>{
+    strType = "Crab"
+    localStorage.setItem("type", strType);
+    console.log(strType);
+});
+
+
+function startGame(){    
+    console.log("StartGame");
+    if (strType.length < 1){
+        alert("Please choose a pet");
+        return false;
+    }
+    strName = document.getElementById("idName").value;
+    if (strName.length < 1){
+      alert("Please type in the name of your pet");
+      return false;
+    }
+
+    localStorage.setItem("name", strName);
+    window.location = "testGameLoop.html"; //or whatever it's called
 }
