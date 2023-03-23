557f4919a818e1c0d1c96d8904f32e242a1de67d
Ray Merrell <RayMerrell@outlook.com>
2023-03-23 15:45:48 +0000

    M46Course

--- assets/js/start.js
+++ assets/js/start.js
@@ -1,5 +1,6 @@
+
 function startGame(){
-    strName= document.getElementById("elName").value;
-    strType= document.getElementById("elType").value; //or whatever to get the data
+    localStorage.setItem("name", document.getElementById("idName").value);
+    localStorage.setItem("type", document.getElementById("idType").value);//or whatever to get the data
     window.location = "MainGame.html"; //or whatever it's called
 }
